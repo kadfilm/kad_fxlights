81b2215c79680c936e5747623b9f6528c51b515d
kadfilm <50627841+kadfilm@users.noreply.github.com>
2024-02-29 00:01:00 +0300

    upload first part

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+author 'KADFILM ||||| https://github.com/kadfilm/kad_fxlights'
+version '1.0'
+
+files {
+    'stream/stg_lights.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/stg_lights.ytyp'
